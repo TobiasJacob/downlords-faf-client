fa67cdd5bfcb998f3754ee9c0610422878df52c6
Aclrian <32142988+Aclrian@users.noreply.github.com>
2020-07-07 22:32:07 +0200

    Revert "Be able to read JsonApiDocument class"
    This reverts commit ed769f63c7c1ead27209aec3a8e9a4de0bc21cc0.

--- src/main/java/com/faforever/client/api/JsonApiMessageConverter.java
+++ src/main/java/com/faforever/client/api/JsonApiMessageConverter.java
@@ -28,19 +28,17 @@ public class JsonApiMessageConverter extends AbstractHttpMessageConverter<Object
   @Override
   protected boolean supports(Class<?> clazz) {
     return Collection.class.isAssignableFrom(clazz)
-        || ReflectionUtils.getTypeName(clazz) != null
-        || clazz.equals(JSONAPIDocument.class);
+        || ReflectionUtils.getTypeName(clazz) != null;
   }
 
   @Override
   @SneakyThrows
+  @SuppressWarnings("unchecked")
   protected Object readInternal(Class<?> clazz, HttpInputMessage inputMessage) {
     try (InputStream inputStream = inputMessage.getBody()) {
       JSONAPIDocument<?> document;
       if (Iterable.class.isAssignableFrom(clazz)) {
         document = resourceConverter.readDocumentCollection(inputStream, Object.class);
-      } else if (clazz.equals(JSONAPIDocument.class)) {
-        return resourceConverter.readDocumentCollection(inputStream, Object.class);
       } else {
         document = resourceConverter.readDocument(inputMessage.getBody(), Object.class);
       }
